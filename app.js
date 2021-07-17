f43c87dba57b86f16ba59fbb9029c90176bb2158
kanishths <kanishthasiyaram15@gmail.com>
2021-07-17 21:11:05 +0530

    Functions added

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 const chalk = require("chalk");
+const { argv } = require("yargs");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
-const msg = getNotes();
-console.log(msg);
-const log = console.log;
 
-log(chalk.blue.bold.italic.inverse("Error"));
+//Customize yargs version
+yargs.version("1.1.0");
+
+//Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
+
+//Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
+
+//Create list command
+yargs.command({
+  command: "list",
+  describe: "List all note",
+  handler: function () {
+    console.log("Listing the notes");
+  },
+});
+
+//Create read command
+yargs.command({
+  command: "read",
+  describe: "Reads a note",
+  handler: function () {
+    console.log("Reading the note");
+  },
+});
+
+yargs.parse();
